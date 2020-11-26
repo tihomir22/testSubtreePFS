7f0616c72aa953b37a3f475cb9a2ecf06b9f7ea0
Tihomir Stoychev <tihomir.stoychev@pfsgroup.es>
2020-11-26 09:29:43 +0100

    añadido nuevo comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CompTestComponent } from './comp-test/comp-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompTestComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/comp-test/comp-test.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CompTestComponent } from './comp-test.component';
+
+describe('CompTestComponent', () => {
+  let component: CompTestComponent;
+  let fixture: ComponentFixture<CompTestComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CompTestComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CompTestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
